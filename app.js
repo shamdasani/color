1fc3c606b66038e42694d0b7929c908edaf6c156
Samay Shamdasani <techboss302@gmail.com>
2016-12-17 22:18:14 -0500

    updates!

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
-// generator function
+  // generator function
   function getColor() {
     return '#' + Math.random().toString(16).slice(2, 8);
   }
-// sets background color style
-  function setBackground() {
-    var bgColor = getColor();
-    document.body.style.background = bgColor;
-    document.getElementById("hex").innerHTML = bgColor;
-  }
-// runs function on space key
-  document.body.onkeyup = function(e){
-      if(e.keyCode == 32){
-        setBackground();
-      }
-  }
 
-// Spacebar message
-  function message(){
-    document.getElementById("message").style.display=" none";
-  }
+  //contrast function
+  function getConstrastingColor() {
+      var hex = getColor();
+      hex = hex.slice(1);
+      r = parseInt(hex.substring(0,2), 16);
+      g = parseInt(hex.substring(2,4), 16);
+      b = parseInt(hex.substring(4,6), 16);
+      r = 255 - r; 
+      g = 255 - g;
+      b = 255 - b;
+      r = r.toString(16);
+      g = g.toString(16);
+      b = b.toString(16);
+      return "#" + r + g + b;
+    }
 
-  window.setTimeout("message();", 1000);
+  // sets background color style
+    function setBackground() {
+      var bgColor = getColor();
+      var txColor = getConstrastingColor();
+      document.body.style.background = bgColor;
+      document.getElementById("color").style.color = txColor;
+      document.getElementById("bg").innerHTML = bgColor;
+      document.getElementById("txt").innerHTML = txColor;
+    }
+
+  // runs function on space key
+    document.body.onkeyup = function(e){
+        if(e.keyCode == 32){
+          setBackground();
+          document.getElementById("message").style.display="none";
+          document.getElementById("color").style.display="inline";
+        }
+    }
+
